89985988caea622a48c48d99b85d7a5cd4ca4756
Yana Kozhokar <yanakozhokar1996@gmail.com>
2023-02-23 14:48:22 +0200

    fixed fetch

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ const fetchImages = (filter, page, per_page) => {
   const BASE_URL = 'https://pixabay.com/api/';
   const KEY = '32921127-0509bb2923ebc5e2476cd7059';
 
-  fetch(
+  return fetch(
     `${BASE_URL}?q=${filter}&key=${KEY}&image_type=photo&orientation=horizontal&page=${page}&per_page=${per_page}`
   ).then(data => data.json());
 };
